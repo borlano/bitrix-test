d5055ba62dc06c5c5176de85818405923849772d
Alexey Arabadzhi <borlano7@gmail.com>
2020-03-19 17:04:20 +0300

    fix

--- local/templates/exam1/components/bitrix/news/rew/bitrix/news.list/.default/template.php
+++ local/templates/exam1/components/bitrix/news/rew/bitrix/news.list/.default/template.php
@@ -12,7 +12,6 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
@@ -21,66 +20,52 @@ $this->setFrameMode(true);
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 	?>
-	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</p>
+<div class="review-block"  id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+  <div class="review-text">
+    <div class="review-block-title">
+        <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+      <span class="review-block-name">
+            <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+            <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></a>
+            <?else:?>
+            <?echo $arItem["NAME"]?>
+            <?endif;?>
+        </span>
+        <?endif;?>
+      <span class="review-block-description"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?>, <?=$arItem["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"]?>, <?= $arItem["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"]?></span>
+    </div>
+
+    <div class="review-text-cont">
+        <?echo $arItem["PREVIEW_TEXT"];?>
+    </div>
+  </div>
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+        <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
+            class="review-img-wrap"
+            border="0"
+            src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+            width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+            height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+            alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+            title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+            style="float:left"
+          /></a>
+        <?else:?>
+        <img
+          class="review-img-wrap"
+          border="0"
+          src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+          width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+          height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+          alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+          title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+          style="float:left"
+        />
+        <?endif;?>
+    <?endif?>
+</div>
 <?endforeach;?>
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
 	<br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>
-</div>
